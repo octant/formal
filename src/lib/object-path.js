ba4ccfaa425c639b059bd5863427cfcc843d0738
Michael Wood <mwood@Michaels-MacBook-Pro.local>
2019-01-05 11:46:52 -0500

    feature: set

--- src/lib/object-path.js
+++ src/lib/object-path.js
@@ -9,3 +9,18 @@ export function get(object, path) {
 
   return pointer[key];
 }
+
+export function set(object, path, value) {
+  const obj = { ...object };
+  const pList = path.split(".");
+  const key = pList.pop();
+
+  const pointer = pList.reduce((accumulator, currentValue) => {
+    if (accumulator[currentValue] === undefined) accumulator[currentValue] = {};
+    return accumulator[currentValue];
+  }, obj);
+
+  pointer[key] = value;
+
+  return obj;
+}
--- src/lib/object-path.test.js
+++ src/lib/object-path.test.js
@@ -1,4 +1,4 @@
-import { get } from "./object-path";
+import { get, set } from "./object-path";
 
 const person = {
   firstName: "Michael",
@@ -25,5 +25,48 @@ describe("get", () => {
 
   it("gets the value of property nested in an array", () => {
     expect(get(person, "vehicles.0.model")).toBe("Sebring");
+  });
+});
+
+describe("set", () => {
+  it("does not modify the original object", () => {
+    const homer = set(person, "firstName", "Homer");
+
+    expect(homer).not.toBe(person);
+    expect(homer.firstName).toBe("Homer");
+  });
+
+  it("sets an existing root property", () => {
+    const homer = set(person, "firstName", "Homer");
+
+    expect(homer.firstName).toBe("Homer");
+  });
+
+  it("sets a new root property", () => {
+    const homer = set(person, "age", 39);
+
+    expect(homer.age).toBe(39);
+  });
+
+  it("sets an existing nested property", () => {
+    const homer = set(person, "address.street", "742 Evergreen Terrace");
+
+    expect(homer.address.street).toBe("742 Evergreen Terrace");
+  });
+
+  it("sets a new nested property", () => {
+    const homer = set(
+      person,
+      "employer.name",
+      "Springfield Nuclear Power Plant"
+    );
+
+    expect(homer.employer.name).toBe("Springfield Nuclear Power Plant");
+  });
+
+  it("sets properties nested inside arrays", () => {
+    const homer = set(person, "vehicles.0.model", "Canyonero");
+
+    expect(homer.vehicles[0].model).toBe("Canyonero");
   });
 });
